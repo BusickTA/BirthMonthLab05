90002283b074383e7d67cc7fae25548ed2513305
thomaa <busickta@mail.uc.edu>
2024-10-10 15:02:22 -0400

    rock paper scissors

--- RPSassignment/src/RPSAssignment.java
+++ RPSassignment/src/RPSAssignment.java
@@ -5,25 +5,30 @@ public class RPSAssignment {
         Scanner in = new Scanner(System.in);
         String player1;
         String player2;
+        String replay;
 
         do {
-            System.out.print("Enter player 1's choice (R, P, or S): ");
-            player1 = in.nextLine();
+            do {
+                System.out.print("Enter player 1's choice (R, P, or S): ");
+                player1 = in.nextLine();
+            } while (!validinput(player1));
 
-        } while
-            (!validinput(player1));
-       //
+            do {
+                System.out.print("Enter player 2's choice (R, P, or S): ");
+                player2 = in.nextLine();
+            } while (!validinput(player2));
 
-        do {
-            System.out.print("Enter player 2's choice (R, P, or S): ");
-            player2 = in.nextLine();
-        } while (!validinput(player2));
-        //System.out.println("Enter R, P, or S.");
+            Winner(player1, player2);
 
-        Winner(player1, player2);
+            System.out.print("Replay? (Y/N): ");
+            replay = in.nextLine();
+        } while (replay.equalsIgnoreCase("Y"));
+
+        System.out.println("Thank you for playing!");
+
     }
 
-  static boolean validinput(String choice) {
+    static boolean validinput(String choice) {
         return choice.equalsIgnoreCase("R") || choice.equalsIgnoreCase("P")
                 || choice.equalsIgnoreCase("S");
     }
@@ -31,18 +36,18 @@ public class RPSAssignment {
     private static void Winner(String player1, String player2) {
         if (player1.equalsIgnoreCase("R")) {
             if(player2.equalsIgnoreCase("R")){
-            System.out.println("It is a tie");
-         }else if (player1.equalsIgnoreCase("R")) {
-            if (player2.equalsIgnoreCase("P")) {
-                System.out.println("Player2 wins, paper covers rock!");
-            } else {
-                System.out.println("Player1 wins, rock beats scissors!");
-            }
+                System.out.println("It is a tie");
+            }else if (player1.equalsIgnoreCase("R")) {
+                if (player2.equalsIgnoreCase("P")) {
+                    System.out.println("Player2 wins, paper covers rock!");
+                } else {
+                    System.out.println("Player1 wins, rock beats scissors!");
+                }
 
-        } else if (player1.equalsIgnoreCase("P")) {
+            } else if (player1.equalsIgnoreCase("P")) {
                 if(player2.equalsIgnoreCase("P"))
                     System.out.println("It is a tie");
-                }
+            }
             if (player2.equalsIgnoreCase("S")) {
                 System.out.println("Player2 wins, scissors cut paper!");
             } else {
@@ -63,4 +68,3 @@ public class RPSAssignment {
         }
     }
 }
-
